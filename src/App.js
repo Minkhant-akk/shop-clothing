a26de701662649e10461c41212fd0e560bcf89a5
Minkhant-akk <sunnycornet123@gmail.com>
2023-11-03 15:51:13 +0630

    broken our homepage into smaller components, finished styling our menu-item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import HomePage  from './home.component';
+import HomePage from './pages/homepage/homepage.components';
 function App() {
   return (
     <div className="App">
